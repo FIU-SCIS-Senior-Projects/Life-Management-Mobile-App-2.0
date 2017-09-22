b38e10dd2244756e9f726b7bfa0053fc8a138a2e
lfern216 <lfern216@fiu.edu>
2017-09-21 22:28:38 -0400

    backup 09/21

---
+++ app/src/main/java/com/example/seniorprojectfall/test/User.java
@@ -0,0 +1,103 @@
+package com.example.seniorprojectfall.test;
+
+/**
+ * Created by lazaro on 9/17/17.
+ */
+
+public class User {
+
+    //String id;
+    String email;
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    String dob;
+    boolean coach;
+    boolean admin;
+
+
+
+
+    public User(){
+
+    }
+
+    public User(String email, String username, String firstName, String lastName, String dob, String password, boolean admin, boolean coach) {
+        this.email = email;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.coach = coach;
+        this.admin = admin;
+        this.password = password;
+    }
+
+
+    public boolean isCoach() {
+        return coach;
+    }
+
+    public void setCoach(boolean coach) {
+        this.coach = coach;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+
+}
